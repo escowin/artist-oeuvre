e8c4f2c0db0aa753657bd0b293df1e6b6b3f6647
escowin <edwin@escowinart.com>
2024-01-20 22:52:52 -0600

    logged terminal init message

---
+++ server.js
@@ -0,0 +1,8 @@
+function init() {
+  const year = new Date().getFullYear();
+  const start = "2024.01.20 Sat 10.20pm";
+
+  console.log(`  begin: ${start}\n  \u00a9${year} Edwin M. Escobar`);
+}
+
+init();
