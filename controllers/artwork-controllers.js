40b07db6ad7a8371eb1896afecfe6013cb737ed3
escowin <edwin@escowinart.com>
2024-01-22 11:43:46 -0600

    included tags in artwork controller POST & PUT methods

--- controllers/artwork-controllers.js
+++ controllers/artwork-controllers.js
@@ -1,4 +1,4 @@
-const { Artwork } = require("../models");
+const { Artwork, Tag, ArtworkTag } = require("../models");
 const { format_dimensions } = require("../utils/helpers");
 
 const artworkController = {
@@ -18,6 +18,13 @@ const artworkController = {
     try {
       const response = await Artwork.findOne({
         where: { id: params.id },
+        include: [
+          {
+            model: Tag,
+            through: ArtworkTag,
+            attributes: ["id", "tag_name"],
+          },
+        ],
       });
 
       !response
@@ -39,6 +46,32 @@ const artworkController = {
         user_id: req.session.user_id,
       });
 
+      // Checks if body includes an array of tags
+      if (req.body.tags && req.body.tags.length) {
+        const artworkTagsArr = req.body.tags.map((tag_id) => {
+          return {
+            artwork_id: response.id,
+            tag_id,
+          };
+        });
+
+        await ArtworkTag.bulkCreate(artworkTagsArr);
+
+        // Retrives the newly created artwork by its id
+        const artwork = await Artwork.findByPk(response.id, {
+          include: [
+            {
+              model: Tag,
+              attributes: { exclude: ["createdAt", "updatedAt"] },
+            },
+          ],
+        });
+
+        // Response for artwork w/ tags
+        return res.json(artwork);
+      }
+
+      // Response for artwork w/o tags
       res.json(response);
     } catch (err) {
       res.status(500).json(err);
@@ -62,9 +95,35 @@ const artworkController = {
         }
       );
 
+      await ArtworkTag.destroy({ where: { artwork_id: req.params.id } });
+
+      // Checks if body includes an array of tags
+      if (req.body.tags && req.body.tags.length) {
+        const artworkTagsArr = req.body.tags.map((tag_id) => {
+          return {
+            artwork_id: req.params.id,
+            tag_id,
+          };
+        });
+
+        await ArtworkTag.bulkCreate(artworkTagsArr);
+      }
+
+      const artwork = await Artwork.findOne({
+        where: { id: req.params.id },
+        include: [
+          {
+            model: Tag,
+            through: ArtworkTag,
+            attribtues: ["id", "tag_name"],
+          },
+        ],
+      });
+
+      console.log(artwork);
       !response
         ? res.status(404).json({ message: "artwork not found" })
-        : res.json(response[1]);
+        : res.json(artwork);
     } catch (err) {
       res.status(500).json(err);
     }
